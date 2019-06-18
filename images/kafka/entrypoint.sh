bdfed764aa53090b46bc769465ada16f19b07982
Craig Andrews <craig.andrews@infinityworks.com>
2019-06-18 16:47:33 +0100

    Automatically create an integration test topic on startup

--- images/kafka/entrypoint.sh
+++ images/kafka/entrypoint.sh
@@ -2,4 +2,31 @@
 
 ADVERTISED_HOSTNAME=$(hostname -i)
 
-exec /entrypoint.old.sh "$@"
+/entrypoint.old.sh "$@" &
+PID=$!
+
+trap "kill $PID" SIGINT SIGTERM
+
+RETRIES=30
+while [[ RETRIES > 0 ]]
+do
+    TOPICS=$(kafka-topics.sh --zookeeper localhost:2181 --list)
+    if [ $? == 0 ]
+    then
+        break
+    fi
+    RETRIES=$((RETRIES-1))
+done
+
+if [[ $RETRIES == 0 ]]
+then
+    echo "Failed to list topics"
+    exit 1
+fi
+
+if [[ ! " ${array[TOPICS]} " =~ " integration-test " ]]
+then
+    kafka-topics.sh --zookeeper localhost:2181 --create --topic integration-test --partitions 10 --replication-factor 1
+fi
+
+wait
