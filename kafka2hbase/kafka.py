5bc77daa1d47b24047e762e26009a6e7b2b63dfe
Craig Andrews <craig.andrews@infinityworks.com>
2019-06-18 13:28:20 +0100

    Exit after 0 messages processed

--- kafka2hbase/kafka.py
+++ kafka2hbase/kafka.py
@@ -9,7 +9,6 @@ from .message import Message
 
 
 _log = logging.getLogger(__name__)
-_log.setLevel(logging.DEBUG)
 
 
 def timestamp():
@@ -17,46 +16,29 @@ def timestamp():
     return datetime.now().timestamp()
 
 
-def sleep_ms(ms):
-    """ Sleep for a number of milliseconds """
-    sleep(ms / 1000)
-
-
-def kafka_stream(consumer, timeout=5):
+def kafka_stream(consumer, timeout=10):
     """ Stream data from kafka until there is no more or the timeout expires """
-    # Wait timeout_ms to see if any messages come in if there are none immediately
+    # Loop continuously until we get no messages during a timeout window
     start = timestamp()
-    topic_messages = None
+    topic_messages = {}
     while not topic_messages and timestamp() - start < timeout:
+        # Try and get some messages, retrying if required
         _log.debug("Waiting for messages from %s", consumer)
-        sleep_ms(10)
         topic_messages = consumer.poll()
+        if topic_messages:
+            break
 
-    # If none arrived then give up
+        sleep(1)
+
+    # If the timeout has been reached then there were never any to start with
     if not topic_messages:
         _log.info("Exceeded %ds waiting for messages", timeout)
-        return
 
     # Generate a sequence of all messages from all topics
     _log.debug("Received topic messages for %d topics", len(topic_messages))
     messages = (message for messages in topic_messages.values() for message in messages)
+
     for message in messages:
-        if not message.topic:
-            _log.warning("message %s missing topic", message)
-            continue
-
-        if not message.key:
-            _log.warning("message %s missing key", message)
-            continue
-
-        if not message.value:
-            _log.warning("message %s missing value", message)
-            continue
-
-        # if not message.timestamp:
-        #     _log.warning("message %s missing timestamp", message)
-        #     continue
-
         wrapped = Message(
             message.topic,
             message.key,
--- kafka2hbase/shovel_stream.py
+++ kafka2hbase/shovel_stream.py
@@ -16,6 +16,11 @@ def qualified_table_name(namespace, prefix, name):
 
 def shovel(stream, store, get_destination):
     """ Shovel data from a stream into a data store until there are none left """
+    messages_processed = 0
+
     for message in stream:
+        messages_processed += 1
         table_name = get_destination(message.topic)
         store(table_name, message.key, message.value, message.timestamp)
+
+    return messages_processed
