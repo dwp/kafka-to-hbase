2a15386d80abdea57ee90c263c13d2358cd0fa11
Craig Andrews <craig.andrews@infinityworks.com>
2019-06-20 11:54:40 +0100

    Add integration test invoke task

--- tasks/test.py
+++ tasks/test.py
@@ -1,10 +1,15 @@
 """ Test invocation tasks """
 
 from invoke import task
-from invoke.exceptions import Exit
 
 
 @task
 def unit(ctx):
     """ Run all unit tests """
     ctx.run("python3 -m nose")
+
+
+@task
+def integration(ctx):
+    """ Run all integration tests """
+    ctx.run("docker-compose run --rm integration-tests behave")
