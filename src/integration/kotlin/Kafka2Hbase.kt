03c2245c7c3f9d8ab65c7e7f094d476765e95375
markmatthews <markmatthews@arkmEnterprises.co.uk>
2019-07-31 14:53:44 +0100

    change the list of topics to a regex, and make integration tests always run

--- src/integration/kotlin/Kafka2Hbase.kt
+++ src/integration/kotlin/Kafka2Hbase.kt
@@ -2,7 +2,6 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import lib.*
 import org.apache.kafka.clients.producer.KafkaProducer
-import java.time.Instant
 
 class Kafka2Hbase : StringSpec({
     configureLogging()
@@ -11,7 +10,7 @@ class Kafka2Hbase : StringSpec({
     val hbase = HbaseClient.connect()
 
     "messages with new identifiers are written to hbase" {
-        val topic = "test-topic-%s".format(Instant.now().toEpochMilli()).toByteArray()
+        val topic = uniqueTopicName()
         val startingCounter = waitFor { hbase.getCount(topic) }
 
         val body = uniqueBytes()
@@ -27,7 +26,7 @@ class Kafka2Hbase : StringSpec({
     }
 
     "messages with previously received identifiers are written as new versions" {
-        val topic = "test-topic-%s".format(Instant.now().toEpochMilli()).toByteArray()
+        val topic = uniqueTopicName()
         val startingCounter = waitFor { hbase.getCount(topic) }
 
         val body = uniqueBytes()
