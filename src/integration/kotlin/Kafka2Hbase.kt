a3060258c845172fc759a4573c2257aeea919159
Nick Green <nick.green@dmwgoup.co.uk>
2019-09-20 14:33:33 +0100

    Applying kotlin language standards

--- src/integration/kotlin/Kafka2Hbase.kt
+++ src/integration/kotlin/Kafka2Hbase.kt
@@ -10,19 +10,19 @@ class Kafka2Hbase : StringSpec({
     val hbase = HbaseClient.connect()
 
     val parser = MessageParser()
-    val convertor = Converter()
+    val converter = Converter()
 
     "messages with new identifiers are written to hbase" {
         val topic = uniqueTopicName()
         val startingCounter = waitFor { hbase.getCount(topic) }
 
         val body = uniqueBytes()
-        val timestamp = convertor.getTimestampAsLong(getISO8601Timestamp())
-        val key = parser.generateKey(convertor.convertToJson(getId().toByteArray()))
+        val timestamp = converter.getTimestampAsLong(getISO8601Timestamp())
+        val key = parser.generateKey(converter.convertToJson(getId().toByteArray()))
         producer.sendRecord(topic, "key1".toByteArray(), body, timestamp)
 
         Thread.sleep(100)
-        val referenceTimestamp = convertor.getTimestampAsLong(getISO8601Timestamp())
+        val referenceTimestamp = converter.getTimestampAsLong(getISO8601Timestamp())
 
         val storedValue = waitFor { hbase.getCellBeforeTimestamp(topic, key, referenceTimestamp) }
         storedValue shouldBe body
@@ -36,15 +36,15 @@ class Kafka2Hbase : StringSpec({
         val startingCounter = waitFor { hbase.getCount(topic) }
 
         val body1 = uniqueBytes()
-        val kafkaTimestamp1 = convertor.getTimestampAsLong(getISO8601Timestamp())
-        val key = parser.generateKey(convertor.convertToJson(getId().toByteArray()))
+        val kafkaTimestamp1 = converter.getTimestampAsLong(getISO8601Timestamp())
+        val key = parser.generateKey(converter.convertToJson(getId().toByteArray()))
         hbase.putVersion(topic, key, body1, kafkaTimestamp1)
 
         Thread.sleep(100)
-        val referenceTimestamp = convertor.getTimestampAsLong(getISO8601Timestamp())
+        val referenceTimestamp = converter.getTimestampAsLong(getISO8601Timestamp())
 
         val body2 = uniqueBytes()
-        val kafkaTimestamp2 = convertor.getTimestampAsLong(getISO8601Timestamp())
+        val kafkaTimestamp2 = converter.getTimestampAsLong(getISO8601Timestamp())
         producer.sendRecord(topic, "key2".toByteArray(), body2, kafkaTimestamp2)
 
         val storedNewValue = waitFor { hbase.getCellAfterTimestamp(topic, key, referenceTimestamp) }
