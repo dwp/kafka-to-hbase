4b2d8d8cee8ac1f34f6386fec2fd99711ee045d1
Daniel Chicot <dan.chicot@gmail.com>
2020-01-15 19:34:47 +0000

    Had to rollback to hbase-client 1.4 as 1.5 does not work with jdk > 9 due to the removoal of class from sun.unsafe package which it uses.
    Also added aws sdk dependency as test only.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.2.2")
-    implementation("org.apache.hbase", "hbase-client", "1.5.0")
+    implementation("org.apache.hbase", "hbase-client", "1.4.9")
     implementation("org.apache.kafka", "kafka-clients", "2.3.0")
     implementation("com.beust", "klaxon", "4.0.2")
     implementation("com.github.everit-org.json-schema", "org.everit.json.schema", "1.12.0")
