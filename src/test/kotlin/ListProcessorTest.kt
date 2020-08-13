276269a2aad7c11a974cbf72171e5656f318eda9
Daniel Chicot <dan.chicot@gmail.com>
2020-08-13 18:34:40 +0100

    More refactoring.

--- src/test/kotlin/ListProcessorTest.kt
+++ src/test/kotlin/ListProcessorTest.kt
@@ -23,10 +23,26 @@ class ListProcessorTest : StringSpec() {
             val consumerRecords = ConsumerRecords<ByteArray, ByteArray>(consumerRecords())
             recordProcessor.processRecords(hbaseClient, consumer, parser, consumerRecords)
             verifyHbaseInteractions(hbaseClient)
-            verifySuccesses(consumer)
-            verifyFailures(consumer)
-            verifyNoMoreInteractions(consumer)
+            verifyKafkaInteractions(consumer)
         }
+    }
+
+    private fun verifyHbaseInteractions(hbaseClient: HbaseClient) {
+        verifyHBasePuts(hbaseClient)
+        verifyNoMoreInteractions(hbaseClient)
+    }
+
+    private fun verifyHBasePuts(hbaseClient: HbaseClient) {
+        val tableNameCaptor = argumentCaptor<String>()
+        val recordCaptor = argumentCaptor<List<HbasePayload>>()
+        verify(hbaseClient, times(10)).putList(tableNameCaptor.capture(), recordCaptor.capture())
+        tableNameCaptor.allValues shouldBe (1..10).map { "database$it:collection$it" }
+    }
+
+    private fun verifyKafkaInteractions(consumer: KafkaConsumer<ByteArray, ByteArray>) {
+        verifySuccesses(consumer)
+        verifyFailures(consumer)
+        verifyNoMoreInteractions(consumer)
     }
 
     private fun verifyFailures(consumer: KafkaConsumer<ByteArray, ByteArray>) {
@@ -40,19 +56,18 @@ class ListProcessorTest : StringSpec() {
             val partition = topicPartition.partition()
             val topicNumber = (index * 2 + 1)
             partition shouldBe 10 - topicNumber
-            topic shouldBe "db.database$topicNumber.collection$topicNumber"
+            topic shouldBe topicName(topicNumber)
         }
 
         verify(consumer, times(5)).seek(topicPartitionCaptor.capture(), positionCaptor.capture())
 
         topicPartitionCaptor.allValues.zip(positionCaptor.allValues).forEachIndexed { index, pair ->
-            println("INDEX: $index")
             val topicNumber = index * 2 + 1
             val topicPartition = pair.first
             val position = pair.second
             val topic = topicPartition.topic()
             val partition = topicPartition.partition()
-            topic shouldBe "db.database$topicNumber.collection$topicNumber"
+            topic shouldBe topicName(topicNumber)
             partition shouldBe 10 - topicNumber
             position shouldBe topicNumber * 10
         }
@@ -64,22 +79,15 @@ class ListProcessorTest : StringSpec() {
         commitCaptor.allValues.forEachIndexed { index, element ->
             val topicNumber = (index + 1) * 2
             element.size shouldBe 1
-            val topicPartition = TopicPartition("db.database$topicNumber.collection$topicNumber", 10 - topicNumber)
+            val topicPartition = TopicPartition(topicName(topicNumber), 10 - topicNumber)
             element[topicPartition] shouldNotBe null
             element[topicPartition]?.offset() shouldBe (topicNumber * 20 * 100) + 1
         }
     }
 
-    private fun verifyHbaseInteractions(hbaseClient: HbaseClient) {
-        val tableNameCaptor = argumentCaptor<String>()
-        val recordCaptor = argumentCaptor<List<HbasePayload>>()
-        verify(hbaseClient, times(10)).putList(tableNameCaptor.capture(), recordCaptor.capture())
-        tableNameCaptor.allValues shouldBe (1..10).map { "database$it:collection$it" }
-    }
-
-    private fun consumerRecords(): Map<TopicPartition, List<ConsumerRecord<ByteArray, ByteArray>>> {
-        return (1..10).associate { topicNumber ->
-            TopicPartition("db.database$topicNumber.collection$topicNumber", 10 - topicNumber) to (1..100).map { recordNumber ->
+    private fun consumerRecords()  =
+        (1..10).associate { topicNumber ->
+            TopicPartition(topicName(topicNumber), 10 - topicNumber) to (1..100).map { recordNumber ->
                 val body = Bytes.toBytes(json(recordNumber))
                 val key = Bytes.toBytes(recordNumber)
                 mock<ConsumerRecord<ByteArray, ByteArray>> {
@@ -89,46 +97,34 @@ class ListProcessorTest : StringSpec() {
                 }
             }
         }
-    }
 
-    private fun messageParser(): MessageParser {
-        return mock<MessageParser> {
-            val hbaseKeys = (1..1000000).map { Bytes.toBytes(it) }
-            on { generateKeyFromRecordBody(any()) } doReturnConsecutively hbaseKeys
-        }
-    }
-
-    private fun kafkaConsumer(): KafkaConsumer<ByteArray, ByteArray> {
-        return mock<KafkaConsumer<ByteArray, ByteArray>> {
-            (1..10).forEach { topicNumber ->
-                on {
-                    committed(TopicPartition("db.database$topicNumber.collection$topicNumber", 10 - topicNumber))
-                } doReturn OffsetAndMetadata((topicNumber * 10).toLong(), "")
+    private fun messageParser() =
+            mock<MessageParser> {
+                val hbaseKeys = (1..1000000).map { Bytes.toBytes(it) }
+                on { generateKeyFromRecordBody(any()) } doReturnConsecutively hbaseKeys
             }
-        }
-    }
 
-    private fun hbaseClient(): HbaseClient {
-        return mock<HbaseClient> {
-            on { putList(any(), any()) } doAnswer {
-                val tableName = it.getArgument<String>(0)
-                val matchResult = Regex("""[13579]$""").find(tableName)
-                if (matchResult != null) {
-                    throw IOException("Table: '$tableName'.")
+    private fun kafkaConsumer() =
+            mock<KafkaConsumer<ByteArray, ByteArray>> {
+                (1..10).forEach { topicNumber ->
+                    on {
+                        committed(TopicPartition(topicName(topicNumber), 10 - topicNumber))
+                    } doReturn OffsetAndMetadata((topicNumber * 10).toLong(), "")
                 }
             }
-        }
-    }
 
-    private fun json(id: Any) =
-        """
-        {
-            "message": {
-                "_id": {
-                    "id": "$id" 
+    private fun hbaseClient() =
+            mock<HbaseClient> {
+                on { putList(any(), any()) } doAnswer {
+                    val tableName = it.getArgument<String>(0)
+                    val matchResult = Regex("""[13579]$""").find(tableName)
+                    if (matchResult != null) {
+                        throw IOException("Table: '$tableName'.")
+                    }
                 }
             }
-        }
-        """.trimIndent()
+
+    private fun json(id: Any) = """{ "message": { "_id": { "id": "$id" } } }"""
+    private fun topicName(topicNumber: Int) = "db.database$topicNumber.collection$topicNumber"
 
 }
